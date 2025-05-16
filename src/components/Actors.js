9b017731f1c89e9456a4669aedc6e55d3312abfa
cwadani75 <cwadani75@gmail.com>
2025-05-16 17:20:21 +0300

    Router lab

--- src/components/Actors.js
+++ src/components/Actors.js
@@ -1,8 +1,23 @@
+// src/components/Actors.js
 import React from "react";
 import { actors } from "../data";
 
 function Actors() {
-  return <div>{/*{code here}*/}</div>;
+  return (
+    <div>
+      <h1>Actors Page</h1>
+      {actors.map((actor) => (
+        <div key={actor.name}>
+          <h3>{actor.name}</h3>
+          <ul>
+            {actor.movies.map((movie, idx) => (
+              <li key={idx}>{movie}</li>
+            ))}
+          </ul>
+        </div>
+      ))}
+    </div>
+  );
 }
 
 export default Actors;
--- src/components/Directors.js
+++ src/components/Directors.js
@@ -1,8 +1,23 @@
+// src/components/Directors.js
 import React from "react";
 import { directors } from "../data";
 
 function Directors() {
-  return <div>{/*{code here}*/}</div>;
+  return (
+    <div>
+      <h1>Directors Page</h1>
+      {directors.map((director) => (
+        <div key={director.name}>
+          <h3>{director.name}</h3>
+          <ul>
+            {director.movies.map((movie, idx) => (
+              <li key={idx}>{movie}</li>
+            ))}
+          </ul>
+        </div>
+      ))}
+    </div>
+  );
 }
 
 export default Directors;
